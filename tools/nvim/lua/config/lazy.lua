5fd77a41bb2efaf7157777d4b8d0353a4fee09d2
sample-usr <1941785+sample-usr@users.noreply.github.com>
2024-12-06 14:25:56 +0100

    Change aerospace config for browser and nix update

--- tools/nvim/lua/config/lazy.lua
+++ tools/nvim/lua/config/lazy.lua
@@ -14,10 +14,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.elixir" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.util.octo" },
     -- import/override with your plugins
     { import = "plugins" },
   },
